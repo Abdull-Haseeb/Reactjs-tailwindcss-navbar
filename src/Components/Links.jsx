fa5ee797630712b5d8fa2641068e1af646f1a157
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-07-08 10:16:56 +0500

    Github Logo is added to the Links Component

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Upwork from "./Upwork";
 import Facebook from "./Facebook";
 import Linkedin from "./Linkedin";
-
+import Github from "./Github";
 export default function Links() {
   return (
     <div className="hidden sm:flex flex-row items-center space-x-6">
+      <Github />
       <Upwork />
       <Facebook />
       <Linkedin />
